aef43d8e8eacf4945bed0304dbfe10552bf1e300
Jhony Reyes <iamjareco@gmail.com>
2022-02-15 20:11:36 +0100

    Archivo reset.css

--- src/core/Application.jsx
+++ src/core/Application.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Title from "core/components/Title/Title";
 import Navbar from "core/components/Navbar/Navbar";
 import { RouterProvider } from "core/routes/RouterProvider";
+import "core/sanitize/reset.css";
 
 const Application = () => {
 	return (
