1d2d8b3277b8db52df8bdfd0878d27a7e5637e37
Loli <lolimurillo5@gmail.com>
2022-02-16 13:05:07 +0100

    Añado imagen en UserProfile

--- src/user/pages/UserProfilePage.jsx
+++ src/user/pages/UserProfilePage.jsx
@@ -1,4 +1,11 @@
 import Title from "core/components/Title/Title";
 
+export const UserProfilePage = () => <Title text="Mi Perfil" />;
 
-export const UserProfilePage = () => <Title text="User profile page" />;
+<div className="Imagen-perfil">
+	<img
+		src="https://thumbs.dreamstime.com/b/vector-leuke-mannequin-girl-selfie-illustration-met-een-leuk-jong-meisje-70515532.jpg"
+		alt="Mi perfil"
+	/>
+	;
+</div>;
