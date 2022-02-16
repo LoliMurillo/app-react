51582daac58a8e245bb4d07e578f8ac3a31f2f32
Loli <lolimurillo5@gmail.com>
2022-02-16 12:01:58 +0100

    Cambios en archivo Application

--- src/core/Application.jsx
+++ src/core/Application.jsx
@@ -1,14 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import Title from "core/components/Title/Title";
 import Navbar from "core/components/Navbar/Navbar";
-import HomePage from "core/pages/HomePage";
+import { RouterProvider } from "./routes/RouterProvider";
+import "core/sanitize/reset.css";
 
 const Application = () => {
 	return (
-		<div>
+		<BrowserRouter>
 			<Title />
 			<Navbar />
-			<HomePage />
-		</div>
+			<RouterProvider />
+		</BrowserRouter>
 	);
 };
 
