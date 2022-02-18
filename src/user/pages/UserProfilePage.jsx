3b50fcf4880478f730b11e8a9b2113bb9b35d234
Loli <lolimurillo5@gmail.com>
2022-02-18 16:53:29 +0100

    Imagen prueba usuario

--- src/user/pages/UserProfilePage.jsx
+++ src/user/pages/UserProfilePage.jsx
@@ -1,8 +1,18 @@
 import Title from "core/components/Title/Title";
 import imgUsuario from "core/pages/img/imgUsuario.jpg";
 
-export const UserProfilePage = () => <Title text="Mi Perfil" />;
+export const UserProfilePage = () => {
+	return (
+		<div className="Imagen-perfil">
+			<h3>Ana Sánchez</h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+				reiciendis dolorum deleniti illum numquam optio repudiandae quia est
+				facilis repellendus, doloribus cupiditate, pariatur obcaecati quam ea.
+				Tenetur explicabo minus quam.
+			</p>
 
-<div className="Imagen-perfil">
-	<img src={imgUsuario} alt="" />
-</div>;
+			<img src={imgUsuario} alt="" />
+		</div>
+	);
+};
