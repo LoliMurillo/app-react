5693c1fd18dad143a26110a455ec0abc5dccebf8
Loli <lolimurillo5@gmail.com>
2022-02-18 13:50:01 +0100

    Prueba Imagen usuario

--- src/user/pages/UserProfilePage.jsx
+++ src/user/pages/UserProfilePage.jsx
@@ -1,11 +1,8 @@
 import Title from "core/components/Title/Title";
+import imgUsuario from "core/pages/img/imgUsuario.jpg";
 
 export const UserProfilePage = () => <Title text="Mi Perfil" />;
 
 <div className="Imagen-perfil">
-	<img
-		src="https://thumbs.dreamstime.com/b/vector-leuke-mannequin-girl-selfie-illustration-met-een-leuk-jong-meisje-70515532.jpg"
-		alt="Mi perfil"
-	/>
-	;
+	<img src={imgUsuario} alt="" />
 </div>;
