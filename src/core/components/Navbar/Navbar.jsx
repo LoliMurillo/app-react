ee8f2cae9e0c75e5a9de90fab6dd8ca47aa6993f
Loli <lolimurillo5@gmail.com>
2022-03-04 13:41:38 +0100

    modificaciongrid

--- src/core/components/Navbar/Navbar.jsx
+++ src/core/components/Navbar/Navbar.jsx
@@ -1,35 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import Grid from "../Grid";
 
 const Navbar = () => {
 	return (
-		<Grid>
-			<div>
-				<header>
-					<h1 className="Title">Relaciona/Tea</h1>
-					<nav>
-						<ul>
-							<li>
-								<Link to={"/"}>Inicio</Link>
-							</li>
-							<li>
-								<Link to={"/profile"}>Perfil</Link>
-							</li>
-						</ul>
-					</nav>
-					<Link className="form-register" to={"/signup"}>
-						Regístrate
-					</Link>
-					<Link className="navbar-access" to={"/signin"}>
-						Inicia sesión
-					</Link>
-					<Link className="about-us" to={"/about"}>
-						Sobre Nosotros
-					</Link>
-				</header>
-			</div>
-		</Grid>
+		<div>
+			<header>
+				<h1 className="Title">Relaciona/Tea</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to={"/"}>Inicio</Link>
+						</li>
+						<li>
+							<Link to={"/profile"}>Perfil</Link>
+						</li>
+					</ul>
+				</nav>
+				<Link className="form-register" to={"/signup"}>
+					Regístrate
+				</Link>
+				<Link className="navbar-access" to={"/signin"}>
+					Inicia sesión
+				</Link>
+				<Link className="about-us" to={"/about"}>
+					Sobre Nosotros
+				</Link>
+			</header>
+		</div>
 	);
 };
 export default Navbar;
